08c72183facc03018928c151c6058ee42722693d
fauzarizky <fauzasmg@gmail.com>
2023-09-17 13:00:27 +0700

    Deploy to Github Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'rizky-portfolio',
+  base: '/rizky-portfolio',
   plugins: [react()],
 })
