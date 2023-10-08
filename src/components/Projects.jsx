1ffd3c7a38758486b82fa3a3c8cfc2d6fcc0d5c1
fauzarizky <fauzasmg@gmail.com>
2023-10-08 17:05:07 +0700

    add new portfolio

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,8 @@
 import { Box, Button, Card, CardBody, CardFooter, Heading, Image, Link, SimpleGrid, Stack, Text, useColorMode } from "@chakra-ui/react";
 import portfolio1 from "../images/mac-view.png";
 import portfolio2 from "../images/mac-view-project-2.png";
-import portfolio3 from "../images/COMING-SOON.png";
+import portfolio3 from "../images/web-advice-quote.png";
+import portfolio4 from "../images/mac-view-weather-app.png";
 
 export default function Projects(props) {
   const { colorMode } = useColorMode();
@@ -53,14 +54,31 @@ export default function Projects(props) {
 
             <Card maxW="sm" minH={'550px'}>
               <CardBody>
-                <Image src={portfolio3} alt="Third Project" borderRadius="lg" h={"35vh"} w={"100%"} />
+                <Image src={portfolio3} alt="Third Project" borderRadius="lg" />
                 <Stack mt="6" spacing="3">
-                  <Heading size="md">Coming Soon</Heading>
-                  <Text>Third Project is under construction!</Text>
+                  <Heading size="md">Advice & Quote Web</Heading>
+                  <Text>My third project is to design and develop a website that generates customized quotes and advice using existing APIs. </Text>
                 </Stack>
               </CardBody>
               <CardFooter>
-                <Link href="#">
+                <Link href="https://fauzarizky.github.io/advice-quote/">
+                  <Button variant="solid" color={"#ffd803"} bgColor={"#272343"} _hover={{color: '#272343', bgColor: '#ffd803'}}>
+                    Visit
+                  </Button>
+                </Link>
+              </CardFooter>
+            </Card>
+            
+            <Card maxW="sm" minH={'550px'}>
+              <CardBody>
+                <Image src={portfolio4} alt="Fourth Project" borderRadius="lg" />
+                <Stack mt="6" spacing="3">
+                  <Heading size="md">Weather App</Heading>
+                  <Text>Create a simple weather application that uses the API from OpenWeather.</Text>
+                </Stack>
+              </CardBody>
+              <CardFooter>
+                <Link href="https://fauzarizky.github.io/weather-app/">
                   <Button variant="solid" color={"#ffd803"} bgColor={"#272343"} _hover={{color: '#272343', bgColor: '#ffd803'}}>
                     Visit
                   </Button>
