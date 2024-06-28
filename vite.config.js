a19e4f26bf2dbc644c3c6613c0135a8cd13f6fcd
fauzarizky <fauzasmg@gmail.com>
2024-06-28 17:41:54 +0700

    Update base path for deployment adjustments

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/rizky-portfolio',
+  base: '/fauzarizky.github.io',
   plugins: [react()],
 })
